207472c7a4550b7e7aca49608f100fdc75f4e00c
Rahul Goswami <rahul.goswami@mintifi.com>
2025-08-10 23:25:59 +0530

    Fixed

---
+++ src/Pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='min-h-[60vh]'>
+        
+    </div>
+  )
+}
+
+export default About
---
+++ src/Pages/Service.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <div className='min-h-[60vh]'>
+
+    </div>
+  )
+}
+
+export default Service
