5f113a8253066492135414fb01232ef17074e76d
Rahul Goswami <rahul.goswami@mintifi.com>
2025-11-20 19:40:33 +0530

    fixed issues

--- server/src/controllers/doctor.controller.js
+++ server/src/controllers/doctor.controller.js
@@ -15,9 +15,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { User } from "../models/user.model.js";
+import { User } from "../models/User.model.js";
 import { Doctor } from "../models/doctor.model.js";
-import { Patient } from "../models/patient.model.js";
+import { Patient } from "../models/Patient.model.js";
 import { Appointment } from "../models/appointment.model.js";
 import { Prescription } from "../models/prescription.model.js";
 import { MedicalRecord } from "../models/medicalRecord.model.js";
